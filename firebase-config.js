bfcdefc4dbf702b64a71ad53cc8c8c509b3e31ce
jukensoudan-route <route.jukensoudan@gmail.com>
2026-07-23 18:52:51 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,3 @@
-// Firebaseコンソールで発行されたWebアプリ設定に置き換えてください。
-// この設定値はFirebaseの仕様上、Webページに置かれる公開用識別情報です。
-// 実際のデータ保護は firestore.rules とログイン認証で行います。
 window.FIREBASE_CONFIG = {
   apiKey: "ここにAPIキー",
   authDomain: "ここにプロジェクトID.firebaseapp.com",
